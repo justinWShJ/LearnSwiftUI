41d85509d3dd241f024195520ae167f4aede0cfc
justin <wangshengjun@hailangtaojin.com>
2019-06-05 14:48:42 +0800

    [feat] Handling User Input

---
+++ HandlingUserInput/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,26 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by ming on 2019/6/5.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
