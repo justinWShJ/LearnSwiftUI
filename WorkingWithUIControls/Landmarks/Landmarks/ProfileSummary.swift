436cdffe19f96c2891929d303a6a8fabad28994f
justin <wangshengjun@hailangtaojin.com>
2019-06-06 11:39:25 +0800

    [feat] working with ui controls

---
+++ WorkingWithUIControls/Landmarks/Landmarks/ProfileSummary.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by ming on 2019/6/6.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary : View {
+    var profile: Profile
+    
+    static let goalFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy"
+        return formatter
+    }()
+    
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(.title)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On" : "Off")")
+            
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalFormat)")
+            
+            VStack(alignment: .leading) {
+                Text("Completed Badges")
+                    .font(.headline)
+                ScrollView {
+                    HStack {
+                        HikeBadge(name: "First Hike")
+                        
+                        HikeBadge(name: "Earth Day")
+                            .hueRotation(Angle(degrees: 90))
+                        
+                        HikeBadge(name: "Tenth Hike")
+                            .grayscale(0.5)
+                            .hueRotation(Angle(degrees: 45))
+                        
+                    }
+                }.frame(height: 140)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Recent Hikes")
+                    .font(.headline)
+                
+                HikeView(hike: hikeData[0])
+            }
+            
+        }
+    }
+}
+
+#if DEBUG
+struct ProfileSummary_Previews : PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+    }
+}
+#endif
---
+++ WorkingWithUIControls/Landmarks/Landmarks/Supporting Views/HikeBadge.swift
@@ -0,0 +1,31 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by ming on 2019/6/6.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeBadge : View {
+    var name: String
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge().frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibility(label: Text("Badge for \(name)."))
+        }
+    }
+}
+
+#if DEBUG
+struct HikeBadge_Previews : PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
+#endif
